62d4d6d34752c63fb3130c3764f8609f3498ccc0
Denis Nuțiu <nutiudenis@hotmail.com>
2016-04-22 20:48:15 +0300

    Updating client

--- chat/tcp_client.py
+++ chat/tcp_client.py
@@ -9,15 +9,15 @@ if len(sys.argv) < 3:
     print("Usage : python {0} hostname port".format(sys.argv[0]))
     sys.exit()
 
-host = sys.argv[1]
-port = int(sys.argv[2])
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(200)
+MASTER_SOCK = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+MASTER_SOCK.settimeout(200)
 
 # connect to remote host
 try:
-    s.connect((host, port))
+    MASTER_SOCK.connect((HOST, PORT))
 except Exception as msg:
     print(type(msg).__name__)
     print("Unable to connect")
@@ -26,12 +26,12 @@ except Exception as msg:
 print("Connected to remote host. Start sending messages")
 
 while True:
-    socket_list = [sys.stdin, s]
+    SOCKET_LIST = [sys.stdin, MASTER_SOCK]
     # Get the list sockets which are readable
-    read_sockets, write_sockets, error_sockets = select.select(socket_list, [], [])
+    READ_SOCKETS, WRITE_SOCKETS, ERROR_SOCKETS = select.select(SOCKET_LIST, [], [])
 
-    for sock in read_sockets: #incoming message from remote server
-        if sock == s:
+    for sock in READ_SOCKETS: #incoming message from remote server
+        if sock == MASTER_SOCK:
             data = sock.recv(4096)
             if not data:
                 print('\nDisconnected from chat server')
@@ -41,4 +41,4 @@ while True:
         else: #user entered a message
             msg = sys.stdin.readline()
             print("\x1b[1A" + "\x1b[2K", end="") # erase last line
-            s.sendall(msg.encode())
+            MASTER_SOCK.sendall(msg.encode())
